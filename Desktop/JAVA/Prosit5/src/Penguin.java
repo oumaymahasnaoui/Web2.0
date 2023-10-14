86a5b59612533cc33f91a0439795c45dd4a0b271
oumaymahasnaoui <oumayma.hasnaoui@esprit.tn>
2023-10-14 16:21:21 +0100

    Prosit5

---
+++ Desktop/JAVA/Prosit5/src/Penguin.java
@@ -0,0 +1,17 @@
+public class Penguin  extends Aquatic {
+
+    private float swimmingDepth;
+
+    public Penguin() {
+    }
+
+    public Penguin(String family, String name, int age, boolean isMammal, String habitat, float swimmingDepth) {
+        super(family, name, age, isMammal, habitat);
+        this.swimmingDepth = swimmingDepth;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", swimmingDepth:" + swimmingDepth;
+    }
+}
